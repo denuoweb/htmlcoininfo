481071d2ddc50242cc9a9bbda02345c428c0b725
Xuan Yan <xuanyan@xuanyan.ws>
2018-01-05 18:31:32 +0800

    contract info

--- packages/qtumscan-node/lib/services/contract/encoding.js
+++ packages/qtumscan-node/lib/services/contract/encoding.js
@@ -4,24 +4,147 @@ const {BufferReader, BufferWriter} = require('qtumscan-lib').encoding
 class Encoding {
   constructor(servicePrefix) {
     this._servicePrefix = servicePrefix
-    this._tokenPrefix = Buffer.from('00', 'hex')
-    this._tokenTransactionPrefix = Buffer.from('01', 'hex')
-    this._tokenTransferPrefix = Buffer.from('02', 'hex')
+    this._contractPrefix = Buffer.from('00', 'hex')
+    this._contractTransactionPrefix = Buffer.from('01', 'hex')
+    this._utxoPrefix = Buffer.from('02', 'hex')
+    this._usedUtxoPrefix = Buffer.from('03', 'hex')
+    this._tokenPrefix = Buffer.from('04', 'hex')
+    this._tokenTransferPrefix = Buffer.from('05', 'hex')
   }
 
-  encodeTokenKey(contract) {
-    return Buffer.concat([this._servicePrefix, this._tokenPrefix, Buffer.from(contract, 'hex')])
+  encodeContractKey(address) {
+    return Buffer.concat([this._servicePrefix, this._contractPrefix, Buffer.from(address, 'hex')])
+  }
+
+  decodeContractKey(buffer) {
+    return {address: buffer.slice(3).toString('hex')}
+  }
+
+  encodeContractValue(txid, owner) {
+    return Buffer.concat([Buffer.from(txid, 'hex'), Buffer.from(owner)])
+  }
+
+  decodeContractValue(buffer) {
+    return {
+      txid: buffer.slice(0, 32).toString('hex'),
+      owner: buffer.slice(32).toString()
+    }
+  }
+
+  encodeContractTransactionKey(address, height = 0, txid = '0'.repeat(64)) {
+    let writer = new BufferWriter()
+    writer.write(this._servicePrefix)
+    writer.write(this._contractTransactionPrefix)
+    writer.writeHexString(address)
+    writer.writeUInt32BE(height)
+    writer.writeHexString(txid)
+    return writer.toBuffer()
+  }
+
+  decodeContractTransactionKey(buffer) {
+    let reader = new BufferReader(buffer)
+    reader.set({pos: 3})
+    let address = reader.readHexString(20)
+    let height = reader.readUInt32BE()
+    let txid = reader.readHexString(32)
+    return {address, height, txid}
+  }
+
+  encodeContractUtxoKey(address, txid = '0'.repeat(64), outputIndex = 0) {
+    let writer = new BufferWriter()
+    writer.write(this._servicePrefix)
+    writer.write(this._utxoPrefix)
+    writer.write(Buffer.from(address))
+    if (Buffer.isBuffer(txid)) {
+      writer.write(txid)
+    } else {
+      writer.writeHexString(txid)
+    }
+    writer.writeUInt32BE(outputIndex)
+    return writer.toBuffer()
+  }
+
+  decodeContractUtxoKey(buffer) {
+    let reader = new BufferReader(buffer)
+    reader.set({pos: 3})
+    let address = reader.read(20).toString('hex')
+    let txid = reader.readHexString(32)
+    let outputIndex = reader.readUInt32BE()
+    return {address, txid, outputIndex}
+  }
+
+  encodeContractUtxoValue(height, satoshis, timestamp) {
+    let writer = new BufferWriter()
+    writer.writeUInt32BE(height)
+    writer.writeDoubleBE(satoshis)
+    writer.writeUInt32BE(timestamp)
+    return writer.toBuffer()
+  }
+
+  decodeContractUtxoValue(buffer) {
+    let reader = new BufferReader(buffer)
+    let height = reader.readUInt32BE()
+    let satoshis = reader.readDoubleBE()
+    let timestamp = reader.readUInt32BE()
+    return {height, satoshis, timestamp}
+  }
+
+  encodeContractUsedUtxoKey(address, txid = '0'.repeat(64), outputIndex = 0) {
+    let writer = new BufferWriter()
+    writer.write(this._servicePrefix)
+    writer.write(this._usedUtxoPrefix)
+    writer.write(Buffer.from(address))
+    if (Buffer.isBuffer(txid)) {
+      writer.write(txid)
+    } else {
+      writer.writeHexString(txid)
+    }
+    writer.writeUInt32BE(outputIndex)
+    return writer.toBuffer()
+  }
+
+  decodeContractUsedUtxoKey(buffer) {
+    let reader = new BufferReader(buffer)
+    reader.set({pos: 3})
+    let address = reader.read(34).toString()
+    let txid = reader.readHexString(32)
+    let outputIndex = reader.readUInt32BE()
+    return {address, txid, outputIndex}
+  }
+
+  encodeContractUsedUtxoValue(height, satoshis, timestamp, outputTxid, spentHeight) {
+    let writer = new BufferWriter()
+    writer.writeUInt32BE(height)
+    writer.writeDoubleBE(satoshis)
+    writer.writeUInt32BE(timestamp)
+    writer.writeHexString(outputTxid)
+    writer.writeUInt32BE(spentHeight)
+    return writer.toBuffer()
+  }
+
+  decodeContractUsedUtxoValue(buffer) {
+    let reader = new BufferReader(buffer)
+    let height = reader.readUInt32BE()
+    let satoshis = reader.readDoubleBE()
+    let timestamp = reader.readUInt32BE()
+    let outputTxid = reader.readHexString(32)
+    let spentHeight = reader.readUInt32BE()
+    return {height, satoshis, timestamp, outputTxid, spentHeight}
+  }
+
+  encodeTokenKey(address) {
+    return Buffer.concat([this._servicePrefix, this._tokenPrefix, Buffer.from(address, 'hex')])
   }
 
   decodeTokenKey(buffer) {
-    return {contract: buffer.slice(3).toString('hex')}
+    return {address: buffer.slice(3).toString('hex')}
   }
 
   encodeTokenValue(name, symbol, decimals, totalSupply) {
     let writer = new BufferWriter()
-    writer.writeUInt8(name.length)
+    writer.writeUInt8(Buffer.from(name).length)
     writer.write(Buffer.from(name))
-    writer.writeUInt8(symbol.length)
+    writer.writeUInt8(Buffer.from(symbol).length)
     writer.write(Buffer.from(symbol))
     writer.writeUInt8(decimals)
     writer.write(totalSupply.toBuffer('be', 32))
@@ -37,25 +160,6 @@ class Encoding {
     let decimals = reader.readUInt8()
     let totalSupply = new BN(reader.read(32), 10, 'be')
     return {name, symbol, decimals, totalSupply}
-  }
-
-  encodeTokenTransactionKey(contract, height, txid) {
-    let writer = new BufferWriter()
-    writer.write(this._servicePrefix)
-    writer.write(this._tokenTransactionPrefix)
-    writer.writeHexString(contract)
-    writer.writeUInt32BE(height)
-    writer.writeHexString(txid)
-    return writer.toBuffer()
-  }
-
-  decodeTokenTransactionKey(buffer) {
-    let reader = new BufferReader(buffer)
-    reader.set({pos: 3})
-    let contract = reader.readHexString(20)
-    let height = reader.readUInt32BE()
-    let txid = reader.readHexString(32)
-    return {contract, height, txid}
   }
 
   encodeTokenTransferKey(txid, index) {
@@ -75,9 +179,9 @@ class Encoding {
     return {txid, index}
   }
 
-  encodeTokenTransferValue(contract, from, to, amount) {
+  encodeTokenTransferValue(address, from, to, amount) {
     let writer = new BufferWriter()
-    writer.writeHexString(contract)
+    writer.writeHexString(address)
     writer.write(Buffer.from(from))
     writer.write(Buffer.from(to))
     writer.write(amount.toBuffer('be', 32))
@@ -86,11 +190,11 @@ class Encoding {
 
   decodeTokenTransferValue(buffer) {
     let reader = new BufferReader(buffer)
-    let contract = reader.readHexString(20)
+    let address = reader.readHexString(20)
     let from = reader.read(34).toString()
     let to = reader.read(34).toString()
     let amount = new BN(reader.read(32), 10, 'be')
-    return {contract, from, to, amount}
+    return {address, from, to, amount}
   }
 }
 
--- packages/qtumscan-node/lib/services/contract/index.js
+++ packages/qtumscan-node/lib/services/contract/index.js
@@ -4,16 +4,19 @@ const qtumscan = require('qtumscan-lib')
 const QtumscanRPC = require('qtumscan-rpc')
 const BaseService = require('../../service')
 const Encoding = require('./encoding')
+const {getAddress} = require('../../utils')
 const BufferUtil = qtumscan.util.buffer
-const {sha256ripemd160, sha256sha256} = qtumscan.crypto.Hash
+const {sha256ripemd160} = qtumscan.crypto.Hash
 const {Contract, tokenABI} = qtumscan.contract
 const Address = qtumscan.Address
 
+const tokenAbi = new Contract(tokenABI)
 const TOKEN_EVENTS = {
-  Transfer: 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-  Approval: '8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925',
-  Mint: '4e3883c75cc9c752bb1db2e406a822e4a75067ae77ad9a0a4d179f2709b9e1f6',
-  TokenPurchase: '623b3804fa71d67900d064613da8f94b9617215ee90799290593e1745087ad18'
+  Transfer: tokenAbi.eventSignature('Transfer').slice(2),
+  Approval: tokenAbi.eventSignature('Approval').slice(2),
+  Mint: tokenAbi.eventSignature('Mint').slice(2),
+  Burn: tokenAbi.eventSignature('Burn').slice(2),
+  TokenPurchase: tokenAbi.eventSignature('TokenPurchase').slice(2)
 }
 const TOKEN_EVENT_HASHES = Object.values(TOKEN_EVENTS)
 
@@ -22,6 +25,7 @@ class ContractService extends BaseService {
     super(options)
     this._block = this.node.services.get('block')
     this._db = this.node.services.get('db')
+    this._transaction = this.node.services.get('transaction')
     this._network = this.node.network
     this._config = options.rpc || {
       user: 'qtum',
@@ -31,18 +35,53 @@ class ContractService extends BaseService {
       port: 3889
     }
     this._client = new QtumscanRPC(this._config)
+    this._contractCache = new LRU(1000)
     this._tokenCache = new LRU(1000)
   }
 
   static get dependencies() {
-    return ['block', 'db']
+    return ['block', 'db', 'transaction']
   }
 
   get APIMethods() {
     return [
+      ['getContract', this.getContract.bind(this), 1],
+      ['getContractTransactions', this.getContractTransactions, 2],
       ['getToken', this.getToken.bind(this), 1],
       ['getTokenTransfers', this.getTokenTransfers.bind(this), 1]
     ]
+  }
+
+  async getContract(address) {
+    let cacheContract = this._contractCache.get(address)
+    if (cacheContract) {
+      return cacheContract
+    }
+    let contractBuffer = await this._db.get(this._encoding.encodeContractKey(address))
+    if (contractBuffer) {
+      let contract = this._encoding.decodeContractValue(contractBuffer)
+      this._contractCache.set(address, contract)
+      return contract
+    }
+  }
+
+  async getContractTransactions(address, {from = 0, to = 0xffffffff} = {}) {
+    let list = []
+    await new Promise((resolve, reject) => {
+      let start = this._encoding.encodeContractTransactionKey(address)
+      let end = Buffer.concat([start.slice(0, -36), Buffer.alloc(36, 0xff)])
+      let utxoStream = this._db.createReadStream({gte: start, lt: end, reverse: true})
+      utxoStream.on('end', resolve)
+      utxoStream.on('error', reject)
+      utxoStream.on('data', async data => {
+        let {txid} = this._encoding.decodeContractTransactionKey(data.key)
+        list.push(txid)
+      })
+    })
+    return {
+      totalCount: list.length,
+      transactions: list.slice(from, to)
+    }
   }
 
   async getToken(address) {
@@ -69,14 +108,14 @@ class ContractService extends BaseService {
       utxoStream.on('error', reject)
       utxoStream.on('data', async data => {
         let {index} = this._encoding.decodeTokenTransferKey(data.key)
-        let {contract, from, to, amount} = this._encoding.decodeTokenTransferValue(data.value)
+        let {address, from, to, amount} = this._encoding.decodeTokenTransferValue(data.value)
         if (from === '0'.repeat(34)) {
           from = null
         }
         if (!token) {
-          token = await this.getToken(contract)
+          token = await this.getToken(address)
           if (token) {
-            token = Object.assign({address: contract}, token)
+            token = Object.assign({address}, token)
           } else {
             resolve()
           }
@@ -94,22 +133,29 @@ class ContractService extends BaseService {
   async onBlock(block) {
     let operations = []
     let contractAddresses = new Set()
+    let utxoMap = new Map()
     for (let tx of block.transactions) {
       for (let i = 0; i < tx.outputs.length; ++i) {
-        let script = tx.outputs[i].script
-        if (script.isContractCreate()) {
-          operations.push(...(await this._processContract(tx, i, block)))
-          break
-        } else if (script.isContractCall()) {
-          let address = script.chunks[4].buf.toString('hex')
+        let output = tx.outputs[i]
+        if (output.script.isContractCreate()) {
+          operations.push(...(await this._processContractCreate(tx, i, block)))
+        } else if (output.script.isContractCall()) {
+          let address = tx.outputs[i].script.chunks[4].buf.toString('hex')
           if (await this.getToken(address)) {
             operations.push({
               type: 'put',
-              key: this._encoding.encodeTokenTransactionKey(address, block.height, tx.id)
+              key: this._encoding.encodeContractTransactionKey(address, block.height, tx.id)
             })
             contractAddresses.add(address)
           }
-          break
+          if (output.satoshis) {
+            operations.push(...this._processOutput(tx, i, block, address, utxoMap))
+          }
+        }
+      }
+      for (let i = 0; i < tx.inputs.length; ++i) {
+        if (tx.inputs[i].script.isContractSpend()) {
+          operations.push(...(await this._processInput(tx, i, block, utxoMap)))
         }
       }
     }
@@ -123,19 +169,29 @@ class ContractService extends BaseService {
     let operations = []
     for (let tx of block.transactions) {
       for (let i = 0; i < tx.outputs.length; ++i) {
-        let script = tx.outputs[i].script
-        if (script.isContractCreate()) {
+        let output = tx.outputs[i]
+        if (output.script.isContractCreate()) {
           operations.push(...(await this._removeContract(tx, i)))
-          break
-        } else if (script.isContractCall()) {
-          let address = script.chunks[4].buf.toString('hex')
+        } else if (output.script.isContractCall()) {
+          let address = output.script.chunks[4].buf.toString('hex')
           operations.push(
             {
               type: 'del',
-              key: this._encoding.encodeTokenTransactionKey(address, block.height, tx.id)
+              key: this._encoding.encodeContractTransactionKey(address, block.height, tx.id)
             },
             ...(await this._removeTokenTransfers(tx, block))
           )
+          if (output.satoshis) {
+            operations.push({
+              type: 'del',
+              key: this._encoding.encodeContractUtxoKey(address, tx.id, index)
+            })
+          }
+        }
+      }
+      for (let i = 0; i < tx.inputs.length; ++i) {
+        if (tx.inputs[i].script.isContractSpend()) {
+          operations.push(...(await this._removeInput(tx, i, block)))
         }
       }
     }
@@ -151,41 +207,133 @@ class ContractService extends BaseService {
     ])).toString('hex')
   }
 
-  async _processContract(tx, index, block) {
+  async _processContractCreate(tx, index, block) {
     if (tx.outputs[index].script.getData().length < 80) {
       return []
     }
     let address = this._getContractAddress(tx, index)
-    let contract = new Contract(tokenABI)
     try {
-      let [{name}, {symbol}, {decimals}, {totalSupply}] = await Promise.all(
-        ['name', 'symbol', 'decimals', 'totalSupply'].map(
-          method => this._callMethod(address, contract, method)
-        )
-      )
-      return [
-        {
-          type: 'put',
-          key: this._encoding.encodeTokenKey(Buffer.from(address, 'hex')),
-          value: this._encoding.encodeTokenValue(name, symbol, decimals.toNumber(), totalSupply)
-        },
-        {type: 'put', key: this._encoding.encodeTokenTransactionKey(address, block.height, tx.id)}
-      ]
+      await this._client.callContract(address, '00')
     } catch (err) {
       return []
     }
+    let owner = await getAddress(tx.inputs[0], this._transaction, this._network)
+    this._contractCache.set(address, {txid: tx.id, owner})
+    let operations = [
+      {
+        type: 'put',
+        key: this._encoding.encodeContractKey(address),
+        value: this._encoding.encodeContractValue(tx.id, owner)
+      },
+      {
+        type: 'put',
+        key: this._encoding.encodeContractTransactionKey(address, block.height, tx.id)
+      }
+    ]
+    try {
+      let [{name}, {symbol}, {decimals}, {totalSupply}] = await Promise.all(
+        ['name', 'symbol', 'decimals', 'totalSupply'].map(
+          method => this._callMethod(address, tokenAbi, method)
+        )
+      )
+      decimals = decimals.toNumber()
+      this._tokenCache.set(address, {name, symbol, decimals, totalSupply})
+      operations.push({
+        type: 'put',
+        key: this._encoding.encodeTokenKey(address),
+        value: this._encoding.encodeTokenValue(name, symbol, decimals, totalSupply)
+      })
+    } catch (err) {}
+    return operations
   }
 
   _removeContract(tx, index) {
     let address = this._getContractAddress(tx, index)
     return [
-      {type: 'del', key: this._encoding.encodeTokenKey(Buffer.from(address, 'hex'))},
-      {type: 'del', key: this._encoding.encodeTokenTransactionKey(address, block.height, tx.id)}
+      {type: 'del', key: this._encoding.encodeContractKey(address)},
+      {type: 'del', key: this._encoding.encodeContractTransactionKey(address, block.height, tx.id)},
+      {type: 'del', key: this._encoding.encodeTokenKey(address)}
+    ]
+  }
+
+  _processOutput(tx, index, block, address, utxoMap) {
+    let output = tx.outputs[index]
+    utxoMap.set(tx.id + ' ' + index, {
+      address,
+      height: block.height,
+      satoshis: output.satoshis,
+      timestamp: block.header.time
+    })
+    return [{
+      type: 'put',
+      key: this._encoding.encodeContractUtxoKey(address, tx.id, index),
+      value: this._encoding.encodeContractUtxoValue(
+        block.height, output.satoshis, block.header.time
+      )
+    }]
+  }
+
+  async _processInput(tx, index, block, utxoMap) {
+    let input = tx.inputs[index]
+    let key = input.prevTxId.toString('hex') + ' ' + input.outputIndex
+    let utxoValue = utxoMap.get(key)
+    if (utxoValue) {
+      utxoMap.delete(key)
+    } else {
+      let transaction = await this._transaction.getTransaction(input.prevTxId.toString('hex'))
+      let output = transaction.outputs[input.outputIndex]
+      utxoValue = {
+        address: output.script.chunks[4].buf.toString('hex'),
+        height: transaction.height,
+        satoshis: output.satoshis,
+        timestamp: block.header.time
+      }
+    }
+    return [
+      {
+        type: 'del',
+        key: this._encoding.encodeContractUtxoKey(
+          utxoValue.address, input.prevTxId, input.outputIndex
+        )
+      },
+      {
+        type: 'put',
+        key: this._encoding.encodeContractUsedUtxoKey(
+          utxoValue.address, input.prevTxId, input.outputIndex
+        ),
+        value: this._encoding.encodeContractUsedUtxoValue(
+          utxoValue.height, utxoValue.satoshis, utxoValue.timestamp, tx.id, block.height
+        )
+      }
+    ]
+  }
+
+  async _removeInput(tx, index, block) {
+    let input = tx.inputs[index]
+    let _tx = await this._transaction.getTransaction(input.prevTxId)
+    let address = _tx.outputs[input.outputIndex].script.chunks[4].buf.toString('hex')
+    return [
+      {
+        type: 'put',
+        key: this._encoding.encodeContractUtxoKey(address, _tx.id, input.outputIndex),
+        value: this._encoding.encodeContractUtxoValue(
+          _tx.__height,
+          _tx.outputs[input.outputIndex].satoshis,
+          _tx.__timestamp
+        )
+      },
+      {
+        type: 'del',
+        key: this._encoding.encodeContractUsedUtxoKey(address, _tx.id, input.outputIndex)
+      }
     ]
   }
 
   async _callMethod(address, contract, method, ...args) {
-    let {executionResult} = await this._client.callContract(address, contract.encodeMethod(method).slice(2))
+    let {executionResult} = await this._client.callContract(
+      address,
+      contract.encodeMethod(method).slice(2)
+    )
     if (executionResult.excepted === 'None') {
       return contract.decodeMethod(method, '0x' + executionResult.output)
     } else {
@@ -206,12 +354,12 @@ class ContractService extends BaseService {
       block.height, block.height,
       JSON.stringify({
         addresses: contracts,
-        topics: [TOKEN_EVENTS.Transfer, TOKEN_EVENTS.Mint]
+        topics: [TOKEN_EVENTS.Transfer, TOKEN_EVENTS.Mint, TOKEN_EVENTS.Burn]
       })
     )
-    for (let {transactionHash, contractAddress, log: logs} of list) {
+    for (let {transactionHash, contractAddress, log} of list) {
       let index = 0
-      for (let {topics, data} of logs) {
+      for (let {topics, data} of log) {
         if (topics[0] === TOKEN_EVENTS.Transfer) {
           let from = new Address(Buffer.from(topics[1].slice(24), 'hex'), this._network).toString()
           let to = new Address(Buffer.from(topics[2].slice(24), 'hex'), this._network).toString()
@@ -223,21 +371,51 @@ class ContractService extends BaseService {
           })
         } else if (topics[0] === TOKEN_EVENTS.Mint) {
           let token = await this.getToken(contractAddress)
+          if (!token) {
+            continue
+          }
           let to = new Address(Buffer.from(topics[1].slice(24), 'hex'), this._network).toString()
           let amount = new BN(this.uint256toBN(data), 16)
-          let {totalSupply} = await this._callMethod(contractAddress, new Contract(tokenABI), 'totalSupply')
+          let {totalSupply} = await this._callMethod(contractAddress, tokenAbi, 'totalSupply')
           token.totalSupply = totalSupply
           operations.push(
             {
               type: 'put',
               key: this._encoding.encodeTokenKey(contractAddress),
-              value: this._encoding.encodeTokenValue(token.name, token.symbol, token.decimals, token.totalSupply)
+              value: this._encoding.encodeTokenValue(
+                token.name, token.symbol, token.decimals, token.totalSupply
+              )
             },
             {
               type: 'put',
               key: this._encoding.encodeTokenTransferKey(transactionHash, index++),
               value: this._encoding.encodeTokenTransferValue(
                 contractAddress, '0'.repeat(34), to, amount
+              )
+            }
+          )
+        } else if (topics[0] === TOKEN_EVENTS.Burn) {
+          let token = await this.getToken(contractAddress)
+          if (!token) {
+            continue
+          }
+          let burner = new Address(Buffer.from(topics[1].slice(24), 'hex'), this._network).toString()
+          let amount = new BN(this.uint256toBN(data), 16)
+          let {totalSupply} = await this._callMethod(contractAddress, tokenAbi, 'totalSupply')
+          token.totalSupply = totalSupply
+          operations.push(
+            {
+              type: 'put',
+              key: this._encoding.encodeTokenKey(contractAddress),
+              value: this._encoding.encodeTokenValue(
+                token.name, token.symbol, token.decimals, token.totalSupply
+              )
+            },
+            {
+              type: 'put',
+              key: this._encoding.encodeTokenTransferKey(transactionHash, index++),
+              value: this._encoding.encodeTokenTransferValue(
+                contractAddress, burner, '0'.repeat(34), amount
               )
             }
           )
