0b34cd3f17df3500dfb84548e5f34519a9ee96a9
Xuan Yan <xuanyan@xuanyan.ws>
2018-02-02 17:48:58 +0800

    fix bug

--- packages/qtuminfo-api/controllers/misc.js
+++ packages/qtuminfo-api/controllers/misc.js
@@ -5,6 +5,7 @@ class MiscController {
     this.node = node
     this.errorResponse = new ErrorResponse({log: this.node.log})
     this._address = this.node.services.get('address')
+    this._header = this.node.services.get('header')
     this._block = this.node.services.get('block')
     this._contract = this.node.services.get('contract')
     this._transaction = this.node.services.get('transaction')
